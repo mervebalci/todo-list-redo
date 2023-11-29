0650097dade743cdb3cfeab71c59dafde3a6ba26
Merve Balci <mervebalci090@gmail.com>
2023-11-29 01:21:44 -0400

    Adding Remove function to delete completed tasks

--- src/App.js
+++ src/App.js
@@ -23,18 +23,24 @@ export default function App() {
       <h1>TODO LIST</h1>
       <input id="userInput" type="text" placeholder="Add a new to do..."></input>
       <button onClick={addTask}>Add</button>
-      <TodoList taskList={tasks} />
+      <TodoList taskList={tasks} setTasks={setTasks}/>
     </div>
   );
 }
 
-function TodoList({ taskList }) {
-  console.log(taskList)
+function TodoList({ taskList, setTasks }) {
+  function removeTask(id) {
+    console.log(taskList)
+    let remainingTasks = taskList.filter(function(task) {
+      return task.id !== id
+    })
+    setTasks(remainingTasks)
+  }
   return (
     <ul>
       {taskList.map((task) =>
         <li key={task.id}>
-          <button></button>
+          <button id={task.id} onClick={() => removeTask(task.id)}></button>
           <span>{task.name}</span>
         </li>
       )}
